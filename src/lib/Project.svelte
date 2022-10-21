ae8c228a1ba4b2ca6c5834cf5ea2d45fca3d348b
Ivan Hart <ihart@ivanhart.dev>
2022-10-20 23:32:14 -0500

    init

---
+++ src/lib/Project.svelte
@@ -0,0 +1,28 @@
+<script>
+export let link = ''
+export let text = ''
+</script>
+
+<a href="{link}"><p>{text}</p></a>
+
+<style>
+p {
+    display: none;
+    margin-block: 10px;
+    color: #000;
+}
+a {
+    width: 100%;
+    max-width: 300px;
+    height: 200px;
+    border: 3px solid #000;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+    text-decoration: none;
+}
+a:hover p {
+    display: block;
+}
+</style>
